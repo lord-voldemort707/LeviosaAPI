1bdff3f0255e785e76855aabde8a921fe51f4b48
lord-voldemort707 <31559531+lord-voldemort707@users.noreply.github.com>
2017-09-03 14:48:42 -0700

    Added ListTeams API

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,12 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+    render json: @teams, status: :ok
+  end
+
   def show
     @team = Team.find_by_code(params[:id])
     render json: @team, status: :ok, include: ['players']
-  end
-
-  def create
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/v1' do
-    resources :teams, only: [:show, :create] do
+    resources :teams, only: [:show, :index] do
       resources :players, only: [:show, :index, :create, :update] do
         resources :player_seasons, only: [:show, :create, :update]
         resources :data_source, only:[:index]
